19b9ad6c8f3ec05d2161fc67df0fde36a486dc1a
ghigbie <gehigbie@gmail.com>
2018-07-23 11:24:43 -0600

    Add footer template

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footer">
+        <h1>Footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data(){
+        return {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
