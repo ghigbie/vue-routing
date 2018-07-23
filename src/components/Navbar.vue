555f9d3700f85a282843b77e244064f683f18991
ghigbie <gehigbie@gmail.com>
2018-07-23 11:37:24 -0600

    Data bind to property

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,8 @@
     <nav class="main-nav">
         <ul>
             <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="about">About</router-link></li>
-            <li><router-link to="contact">Contact Us</router-link></li>
+            <li><router-link :to="{ name: 'About'}">About</router-link></li>
+            <li><router-link :to="{ name: 'Contact'}">Contact Us</router-link></li>
         </ul>
     </nav>
     
