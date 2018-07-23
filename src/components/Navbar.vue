48619bc5ea2d69c9008e0024282cbc605d96f5c1
ghigbie <gehigbie@gmail.com>
2018-07-23 11:40:50 -0600

    Add Why us template

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="main-nav">
         <ul>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link :to="{ name: 'About'}">About</router-link></li>
-            <li><router-link :to="{ name: 'Contact'}">Contact Us</router-link></li>
+            <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+            <li><router-link :to="{ name: 'About' }">About</router-link></li>
+            <li><router-link :to="{ name: 'Contact' }">Contact Us</router-link></li>
         </ul>
     </nav>
     
---
+++ src/components/Why.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="why">
+        <h1>Why us?</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Why',
+    data() {
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
