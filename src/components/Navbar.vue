8eed3eea8101cdd7ea404666fddde8d618b9ee4a
ghigbie <gehigbie@gmail.com>
2018-07-23 11:17:57 -0600

    Customize component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,28 @@
 <template>
+    <nav class="main-nav">
+        <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="about">About</router-link></li>
+            <li><router-link to="contact">Contact Us</router-link></li>
+        </ul>
+    </nav>
     
 </template>
 
 <script>
 export default {
-    
+    name: 'Navbar',
+    data() {
+        return{
+            
+        }
+    }
 }
 </script>
 
 <style scoped>
+    nav li{
+        list-style: none;
+    }
 
 </style>
