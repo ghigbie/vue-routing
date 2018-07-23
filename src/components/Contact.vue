7dcbefe6157e86631461410d1877cb5e9a0090ab
ghigbie <gehigbie@gmail.com>
2018-07-23 11:32:13 -0600

    Register contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="contact">
-        <h1>Contact us<h1>
+        <h1>Contact us</h1>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import About from '@/components/About';
+import Contact from '@/components/Contact';
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     }
   ]
 })
