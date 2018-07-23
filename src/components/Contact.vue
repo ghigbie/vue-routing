5797a67ba1b00c019103bf2c9b548fe841db2172
ghigbie <gehigbie@gmail.com>
2018-07-23 11:30:32 -0600

    Add template

---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contact">
+        <h1>Contact us<h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+    data(){
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
