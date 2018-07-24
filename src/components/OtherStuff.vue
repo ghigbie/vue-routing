941d4e65e16581128ea2aebc0992fea5ee0e80fd
ghigbie <gehigbie@gmail.com>
2018-07-24 13:43:34 -0600

    Add other stuff template

---
+++ src/components/OtherStuff.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="other-stuff">Other Stuff</div>
+</template>
+
+<script>
+export default {
+    name: 'OtherStuff',
+    data(){
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
