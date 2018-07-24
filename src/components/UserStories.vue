e7c3359eae5f48660ffa903b4509260b6cf6b80a
ghigbie <gehigbie@gmail.com>
2018-07-24 09:21:35 -0600

    Add template for user stories component

---
+++ src/components/UserStories.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user-stories">
+        <h1>User Stories {{ id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserStories',
+    data(){
+        return{
+            storyId: ['1', '2', '3', '5']
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
