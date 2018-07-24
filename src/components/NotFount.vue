b651384d81b15af4274b64ebe68436f1d2a849a5
ghigbie <gehigbie@gmail.com>
2018-07-23 19:38:13 -0600

    Add template for component

---
+++ src/components/NotFount.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="not-found">
+        <h1>This page is not found</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+    data(){
+        return {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
