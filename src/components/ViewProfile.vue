8db0830cd8e078be2abe3ea80611dad5b740d0b0
ghigbie <gehigbie@gmail.com>
2018-07-23 18:43:40 -0600

    Add view profile

---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="view-profile">
+        <h1>View Profile</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ViewProfile',
+    data() {
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
