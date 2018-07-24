ca9fb6ae5b49b26a4ba8de213a87391beb4d1e84
ghigbie <gehigbie@gmail.com>
2018-07-24 08:58:49 -0600

    Add comments component

---
+++ src/components/Comments.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="comments">
+        <h1>Comments</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Comments',
+    data(){
+        return {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
